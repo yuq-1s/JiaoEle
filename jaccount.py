939e3739176d3488879dd89b896000169d274437
yuq <zxdewr@sjtu.edu.cn>
2016-11-11 23:42:50 +0800

    Fix: ASP.NET_SessionId not authorized after log in

--- jaccount.py
+++ jaccount.py
@@ -1,8 +1,16 @@
 #! /usr/bin/env python3
 
+'''
+    Login jaccount and return requests.session object.
+
+    TODO: 
+        1. Add argparse to process username and password from CLI
+        2. Decode captcha automatically
+'''
+
 import requests
 import shutil
-import urllib as ul
+import sys
 from pdb import set_trace
 from bs4 import BeautifulSoup
 from PIL import Image
@@ -20,30 +28,55 @@ def captcha_src(soup):
 
 
 def input_captcha(sess, soup):
-    cap_src = captcha_src(soup)
-    r = sess.get('https://jaccount.sjtu.edu.cn/jaccount/'+cap_src, stream=True)
+    r = sess.get('https://jaccount.sjtu.edu.cn/jaccount/'+captcha_src(soup),
+            stream=True)
     img = Image.open(BytesIO(r.content))
     img.show()
     return input('What\'s in the image?\n')
 
-def post_data(sess, soup):
+def post_data(sess, soup, user, secret):
     form = ['sid', 'returl', 'se', 'v']
 
     data = dict(zip(form, [soup.find('input', {'name': s}).get('value') for s in form]))
-    data['user'] = 'username'
-    data['pass'] = 'password'
+    data['user'] = user
+    data['pass'] = secret
     data['captcha'] = input_captcha(sess, soup)
     return data
 
-def login(user, password):
+def login(user, secret):
+    # Get Session object
     sess = requests.Session()
+
+    # Store ASP.NetSessionId in sess
     resp = sess.get('http://electsys.sjtu.edu.cn/edu/login.aspx')
+
+    # Prepare for post
     soup = BeautifulSoup(resp.text, 'html.parser')
     post_url = 'https://jaccount.sjtu.edu.cn/jaccount/ulogin'
+    
+    # Post username and password and get authorization url
+    auth_resp = sess.post(post_url, data = post_data(sess, soup, user, secret))
+    soup = BeautifulSoup(auth_resp.text, 'html.parser')
+    auth_url = soup.find('meta', {'http-equiv':'refresh'})['content'].split('url=')[1]
 
-    resp = sess.post(post_url, data = post_data(sess, soup))
-    return sess
+    # Get authorized
+    sess.get(auth_url)
 
+    return sess# , prepare_form(sess)
+
+# def check_session(sess):
+#     check_list = ['ASP.NET_SessionId', 'JASiteCookie', 'mail_test_cookie']
+#     set_trace()
+#     resp_check = sess.get('http://electsys.sjtu.edu.cn/edu/student/sdtinfocheck.aspx',
+#             cookies = {s: sess.cookies[s] for s in check_list})
+#     set_trace()
+# def prepare_form(sess):
+#     form_url = 'http://electsys.sjtu.edu.cn/edu/newsboard/newsinside.aspx'
+#     form_soup = BeautifulSoup(sess.get(form_url).text, 'html.parser')
+#     return {inp['name']: inp['value'] for inp in form_soup.find_all('input')}
+
+if __name__ == '__main__':
+    print(login(sys.argv[1], sys.argv[2]).cookies)
 # new_soup = BeautifulSoup(resp.text, 'html.parser')
 
 # print(resp.cookies)
--- sdtMain.py
+++ sdtMain.py
@@ -1,39 +1,40 @@
 #! /usr/bin/env python3
-
-import requests
+from jaccount import login
+from pdb import set_trace
 from bs4 import BeautifulSoup
+import requests
 
-def tohttps(oriurl):
-    prot, body = edu_resp.request.url.split(':')
-    return prot + 's' + body
+# When in main page (aka path /)
+TARGET_URL = 'http://electsys.sjtu.edu.cn/edu/'
+ASP_FORM_ID = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION']
+def autition(sess):
+    # xklc = 1: 海选
+    # xklc = 2: 抢选
+    # xklc = 3: 第三轮选
+    xkurl = TARGET_URL+'student/elect/electwarning.aspx'+'?xklc=1'
 
-def get_captcha_src(soup):
-    for img_tag in soup.findAll('img'):
-        if img_tag['src'].startswith('captcha?'):
-            return img_tag['src']
-    raise "Cannot find captcha."
+    # Get this site also work ???
+    # http://electsys.sjtu.edu.cn/edu/student/elect/electcheck.aspx?xklc=1
+    resp = sess.post(xkurl, data = prepare_form('?xklc=1'))
 
-hd = {  # 'Cookie': '''ASP.NET_SessionId=j2cywsrkdvyxin55534yo1i2;
-        # mail_test_cookie=IJIBHIAK''',
-        'Host': 'electsys.sjtu.edu.cn',
-        # 'Upgrade-Insecure-Requests': 1,
-        'Cookie':
-        'ASP.NET_SessionId=j2cywsrkdvyxin55534yo1i2;JASiteCookie=CA4KehZCYxWVFyEAXjBRrZRBJujp0rJkec9L1bSM+9+Psk1aJ+amIxP5EUYaqlVVBP+tEubIulfUF8KnfLHv/jNsgmB5VmVW+EfcQBsnoxDf7Vmux3wqXFMZJmPdbJb0FdZJC+sORIyhKa47CX+Lz2VD8erMRRqgaiX5+8j1sLtnhWvP+yk0FMP0xawbN2/dNeGB8e6pXdqmanLIb4jL6rnYIRNZkh73MgLFu6LDaOOPKB2M8O4NWfUymduMAqkUrOud/cw+tC0oyABHWFG57XOZ2DRFpfq28J6RBlbn5d/xFKsVMQhh76KKFcGC4e7ErT3p3kej1qb2hZYDOrgVSclKBCrpKly4n3qulA1TjTm0gNNCmVqR2LVknCwAG2ZPLiK20xMUXqbM4ar7eWL+V44w7iZDOF/3FL/L4IJkZ8AdmNE8qFmbftI;mail_test_cookie=IJIBHIAK',
-        'Accept': '''text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8''',
-        }
-edu_resp = requests.get('http://electsys.sjtu.edu.cn/edu/student/sdtMain.aspx', headers =
-        hd)
-edu_soup = BeautifulSoup(edu_resp.text, 'html.parser')
-# print(edu_soup.findAll("div", { "class" : "captcha-input" }))
-# print(edu_soup.findAll("img")[1])#.split('"'))
-print(edu_soup.prettify())
-# print(getcaptcha(edu_soup))
-# print(edu_soup.findAll("img")[1]['src'])#.split('"'))
+def prepare_form(query_str):
+    sess = requests.Session()
+    form_url = TARGET_URL + 'student/elect/electwarning.aspx' + query_str
+    form_soup = BeautifulSoup(sess.get(form_url).text, 'html.parser')
+    form = {inp['name']: inp['value'] for inp in form_soup.find_all('input') if
+            inp['name'] in ASP_FORM_ID}
+    form['CheckBox1'] = 'on'
+    form['btnContinue'] = '继续'
+    assert len(form) == 5
+    return [(s, form[s]) for s in ['__VIEWSTATE', '__VIEWSTATEGENERATOR', 
+        '__EVENTVALIDATION', 'CheckBox1', 'btnContinue']]
 
-# print(edu_resp.status_code)
-# print(edu_resp.headers)
-# print(edu_soup.prettify())
-# url2 = tohttps(edu_resp.request.url)
+    # form_url = 'http://electsys.sjtu.edu.cn/edu/newsboard/newsinside.aspx'
 
-# resp2 = requests.get('http://electsys.sjtu.edu.cn/edu/login.aspx', headers = hd)
-# print(BeautifulSoup(resp2.text, 'html.parser').prettify())
+def prepare_cookie(sess):
+    ele_cookies_list = ['ASP.NET_SessionId', 'mail_test_cookie']
+    return {s: sess.cookies[s] for s in ele_cookies_list}
+
+if __name__ == '__main__':
+    sess = login('username', 'secret')
+    autition(sess)
