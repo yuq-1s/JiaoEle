f8f4da410c1f5959856f5ec9aa093952e955f33d
yuq <zxdewr@sjtu.edu.cn>
2016-12-08 21:59:24 +0800

    Fix: Some typo in select.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python3
 import jaccount, stdMain
+from course_collector.course_collector.spider.tongshi import TongshiSpider
 
 def main():
-    cookie = jaccount.login()
-    print(cookie)
 
 
