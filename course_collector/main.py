5887bc17b833352c513f19df67e6023ba4cad6bf
yuq <zxdewr@sjtu.edu.cn>
2016-12-11 17:14:34 +0800

    Add: Query by time of non-overlapping courses.

--- course_collector/main.py
+++ course_collector/main.py
@@ -53,34 +53,71 @@ class Main(object):
                 pass
             process.crawl(TongShiSpider, user, passwd)
             need_restart = True
+            logger.info('Preparing to update selected courses...')
         if not COURSES_PATH.is_file():
             process.crawl(CurrentSpider)
             need_restart = True
+            logger.info('Preparing to update available courses...')
         if need_restart:
+            logger.info('Updating...')
             process.start()
             os.execl(sys.executable, sys.executable, *sys.argv)
 
         self.current = load_file(get_feed_path(CurrentSpider))
         self.courses = load_file(get_feed_path(TongShiSpider))
-
-        self.ls_all()
+        logger.info('Initialization succeeded.')
 
     def ls_all(self):
         for c in self.courses:
             print(c['name'])
 
     def get_all_non_overlap(self):
-        return [c for c in self.courses for curr in self.current if not
-                are_overlap(c, curr)]
+        return {c for c in self.courses for curr in self.current if not
+                are_overlap(c, curr)}
 
         # for course in self.courses:
         #     for curr in self.current:
         #         if not are_overlap(course, curr):
         #             print(course['name'])
 
-    def non_overlap_by_time(self, courses):
-        '''Usage: <2, 1, 3[, 1, 8][, 'o']>[...]
+    def non_overlap_by_time(self, raw):
+        ''' Usage: <2, 1, 3[, 1, 8][, 'o']>[;...]
+            @return: Course names that do not overlap with any queried period,
+                    without considering current selected courses.
         '''
+        def parse_query(raw):
+            queried = []
+            for period in raw.split(';'):
+                try:
+                    r = re.match('(?:\s*(\d)\s+(\d+)\s+(\d+)\s*(?:(\d+)\s+(\d+)\s*)?([oe]?))')
+                    if len(r) != 6: continue
+                    if not any(r): continue
+                    ret = {}
+                    ret['wbegin'] = int(r[3]) if r[3] else 1
+                    ret['wend'] = int(r[4]) if r[4] else 16
+                    ret['wbegin'] = r[5] if r[5] else 'oe'
+                    assert r[0] and 1 <= r[0] <= 7, 'Weekday must in range 1-7'
+                    assert r[1] and 1 <= r[1] <= 14, 'Course period must be in range 1-14'
+                    assert r[2] and 1 <= r[2] <= 14, 'Course period must be in range 1-14'
+                    assert 1 <= ret['wbegin'] <= 16, 'Week period must in range 1-16'
+                    assert 1 <= ret['wend'] <= 16, 'Week peroid must in range 1-16'
+                    r[1], r[2] = min(r[1], r[2]), max(r[1], r[2])
+                    ret['weekday'] = r[0]
+                    ret['cbegin'] = r[1]
+                    ret['cend'] = r[2]
+                    queried.append(ret)
+                except AssertionError as e:
+                    logger.warn(e)
+                    logger.warn('Ignored: %s', period)
+            return queried
+
+        # TODO: add usage info
+        queried = parse_query(raw)
+        if not queried:
+            logger.warn('Invalid input, nothing to query.')
+            return set()
+        return {c['name'] for c in self.courses for q in queried if not
+                are_overlap(c, q)}
 
 # TODO: argparse
 def main():
