7b35eec39cf944154bea910eb75a9fb6a3c9c870
yuq <zxdewr@sjtu.edu.cn>
2016-12-18 16:19:38 +0800

    Add: qiangke utility to main.py

--- course_collector/main.py
+++ course_collector/main.py
@@ -10,7 +10,9 @@ from course_collector.spiders.tongshi import TongShiSpider
 from logging import getLogger
 from twisted.internet import reactor
 from pdb import set_trace
+from functools import wraps
 
+import xpinyin
 import copy
 import time
 import json
@@ -21,14 +23,13 @@ import cmd
 
 logger = getLogger(__name__)
 
-# class Course(object):
-#     def __init__(self, course):
-#         self.course = course
-#         # self.odd = [{o['weekday']: course['odd_week']
-#         # self.even = course['even_week']
+EDU_URL = 'http://electsys.sjtu.edu.cn/edu/'
+ELECT_URL = EDU_URL+'student/elect/'
 
-#     def contains(self, other):
-#         all(other.course
+def prepare_cookie(sess):
+    ele_cookies_list = ['ASP.NET_SessionId', 'mail_test_cookie']
+    cookies = {s: sess.cookies[s] for s in ele_cookies_list}
+    return [{'name': key, 'value': val, 'path': '/edu'} for key, val in cookies.items()]
 
 def are_overlap(course1, course2):
     def have_overlap(low1, high1, low2, high2):
@@ -62,30 +63,6 @@ def contains(course1, course2):
             return False
     return True
 
-    # def interval_contains(low1, high1, low2, high2):
-    #     low1 = int(low1)
-    #     high1 = int(high1)
-    #     low2 = int(low2)
-    #     high2 = int(high2)
-    #     assert low1 <= high1 and low2 <= high2
-    #     return low1 <= low2 and high1 >= high2
-
-    # return all(c1['weekday'] == c2['weekday'] \
-    #         and interval_contains(c1['wbegin'], c1['wend'], c2['wbegin'],c2['wend'])\
-    #         and interval_contains(c1['cbegin'], c1['cend'], c2['cbegin'],c2['cend'])\
-    #         for c1 in course1[parity] for c2 in course2[parity] \
-    #         for parity in ('odd_week', 'even_week'))
-
-    # for parity in ('odd_week', 'even_week'):
-    #     for c1 in course1.get(parity, {}):
-    #         for c2 in course2.get(parity, {}):
-    #             if c1['weekday'] != c2['weekday'] \
-    #                     or not interval_contains(c1['wbegin'], c1['wend'], c2['wbegin'],c2['wend'])\
-    #                     or not interval_contains(c1['cbegin'], c1['cend'], c2['cbegin'],c2['cend']):
-    #                         return False
-    # return True
-
-
 def get_feed_path(spider):
     return spider.custom_settings['FEED_URI'].split('file://')[1]
 
@@ -93,45 +70,98 @@ def load_file(filename):
         with open(filename, 'r') as f:
             return json.load(f)
 
-# TODO: Make Spiders share the cookie middleware.
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    @classmethod
+    def recover(cls, func):
+        @wraps(func)
+        def wrapper(self, *args, **kw):
+            self.driver.get(self.recover_url)
+            return func(self, *args, **kw)
+        return wrapper
+
+                # except NoSuchElementException:
+                #     logging.warn('Not in CommonCourse page, nagivating to it...')
+                #     self.driver.get(ELECT_URL+'speltyCommonCourse.aspx')
+
+class InitPage(BasePage):
+    recover_url = ELECT_URL+'electwarning.aspx?xklc=1'
+
+    @BasePage.recover
+    def init(self):
+        self.driver.find_element_by_id('CheckBox1').click()
+        self.driver.find_element_by_id('btnContinue').click()
+        sleep(0.1)
+        assert 'speltyR' in self.driver.current_url, 'init failed'
+
+class TongshiPage(BasePage):
+    recover_url = ELECT_URL+'speltyCommonCourse.aspx'
+
+    @BasePage.recover
+    def get_courses_list(self, course_type):
+        ct = ['rw', 'sk', 'zk', 'sx'].index(course_type)+2
+        self.driver.find_element_by_xpath('//*[@id="gridGModule_ctl0%s_radioButton"]'%ct).click()
+        sleep(0.1)
+        assert '/speltyCommonCourse' in self.driver.current_url, 'get course list failed.'
+
+
+    def get_name_by_cid(self, cid):
+        return self.driver.find_element_by_xpath('//td[contains(text(),"%s")]/../td[1]'%cid).text
+
+    def get_radio_by_cid(self, cid):
+        return self.driver.find_element_by_xpath('//input[@value="%s"]'%cid)
+
+    def view_lesson_by_cid(self, cid):
+        self.get_radio_by_cid(cid).click()
+        self.driver.find_element_by_id('lessonArrange').click()
+        sleep(0.1)
+        assert '/viewLessonArrange' in self.driver.current_url, 'view lesson failed'
+
+    def submit(self):
+        self.driver.find_element_by_id('btnSubmit').click()
+
+class LessonPage(BasePage): 
+    def get_teacher_by_bsid(self, bsid):
+        return self.driver.find_element_by_xpath('//input[value="%s"]/../../../td[1]')
+
+    def select_by_bsid(self, bsid):
+        self.driver.find_element_by_xpath('//input[@value="%s"]'%bsid).click()
+        self.driver.find_element_by_id('LessonTime1_btnChoose').click()
+        # assert 'speltyCommonCourse' in self.driver.current_url, 'select failed.'
+
+class MessagePage(BasePage):
+    def back(self):
+        try:
+            self.driver.find_element_by_id('btnContinue').click()
+        except NoSuchElementException:
+            self.driver.find_element_by_id('Button1').click()
+
+        # self.driver.back()
+
+    def handle_message(self):
+        # if 'message=' not in self.driver.current_url: return
+        message = unquote(self.driver.current_url.split('message=')[1])
+        if '刷新' in message: 
+            sleep(1)
+        logging.error(message)
+        self.back()
+
 class Main(cmd.Cmd):
     CURRENT_PATH = get_feed_path(CurrentSpider) # Path(get_feed_path(CurrentSpider))
     COURSES_PATH = get_feed_path(TongShiSpider) # Path(get_feed_path(TongShiSpider))
     EXPIRE_SEC = 900
+    intro = '''Here is the evil software to grab courses of SJTU. Have fun ;)'''
     prompt = '>> '
-    def __init__(self, user = 'a', passwd = 'b'):
+
+    def __init__(self):
         super().__init__()
-        process = CrawlerProcess(get_project_settings())
-        need_restart = False
-
-        # if not self.CURRENT_PATH.is_file() or \
-        #         time.time()-self.CURRENT_PATH.stat().st_mtime>self.EXPIRE_SEC:
-        #     # try:
-        #     #     os.remove(self.CURRENT_PATH)
-        #     # except FileNotFoundError:
-        #     #     pass
-        #     process.crawl(CurrentSpider)
-        #     need_restart = True
-        #     logger.info('Preparing to update selected courses...')
-# #         if not self.COURSES_PATH.is_file():
-# #             process.crawl(CurrentSpider)
-# #             need_restart = True
-# #             logger.info('Preparing to update available courses...')
-        # if need_restart:
-        #     logger.info('Updating...')
-        #     process.start()
-        #     os.execl(sys.executable, sys.executable, *sys.argv)
-
+        self.py = xpinyin.Pinyin()
         self.current = load_file(self.CURRENT_PATH)
         self.courses = load_file(self.COURSES_PATH)
-        # tmp = load_file(get_feed_path(TongShiSpider))
-        # sunzi = copy.deepcopy(tmp[0])
-        # last = tmp[0]
-        # for i, c in enumerate(tmp[1:]):
-        #     tmp[i-1]['name'] = c['name']
-        # tmp[-1]['name'] = '工程可持续发展'
-        # self.courses = [sunzi]+tmp
-        # logger.info('Initialization succeeded.')
+        self.user = sys.argv[1]
+        self.passwd = sys.argv[2]
 
     @staticmethod
     def parse_query(raw):
@@ -174,13 +204,45 @@ class Main(cmd.Cmd):
 
         return {'odd_week': odd, 'even_week': even}
 
-    def ls_all(self):
-        for c in self.courses:
-            print(c['name'])
 
-    def get_all_non_overlap(self):
-        return {c['name'] for c in self.courses for curr in self.current if not
-                are_overlap(c, curr)}
+    def __to_letter(self, s):
+        return re.sub(r'[^\w\s]', '', self.py.get_initials(s, '')).lower()
+
+
+    def do_qname(self, arg):
+        ''' Print out courses based on name query.
+            Support pinyin abbreviation.
+            Example: >> qname ty4
+                    for query of 体育（4）
+        '''
+        print([c for c in self.courses if self.__to_letter(c['name']) == arg \
+                or c['name'] == arg])
+
+    def complete_qname(self, text, line, begidx, endidx):
+        ''' Auto-completion utility for qname.
+            With support for pinyin.
+        '''
+        if not text:
+            completions = self.courses[:]
+        else:
+            completions = [c['name'] for c in self.courses 
+                    if self.__to_letter(c['name']).startswith(text)]
+
+        return [c['name'] for c in self.courses if \
+                self.__to_letter(c['name']).startswith(self.__to_letter(text)) \
+                ] if text else self.courses
+
+    def do_ls(self, _):
+        ''' List out all courses'''
+        for name in set(c['name'] for c in self.courses):
+            print(name)
+
+    def do_qavai(self, _):
+        ''' Print out avalible courses without overlapping with currently
+            selected courses
+        '''
+        print({c['name'] for c in self.courses for curr in self.current if not
+                are_overlap(c, curr)})
 
         # for course in self.courses:
         #     for curr in self.current:
@@ -219,21 +281,193 @@ class Main(cmd.Cmd):
         print('Bye')
         return True
 
-# TODO: argparse
-def main():
-    process = CrawlerProcess(get_project_settings())
-    print('Start')
-    process.crawl('current')
-    process.start()
-    print('End')
-    # course_info_file = Path('data/courses.json')
-    current_courses = Path('/tmp/yuq_test_spider.json')
-    with open('/tmp/yuq_test_spider.json', 'r') as f:
-        courses = json.load(f)
-    print(courses)
-    # if not course_info_file.is_file():
-    #     # TODO: Try to download the file from peers.
-    #     print("Scraping target website.")
+    def do_EOF(self, _):
+        while True:
+            inp = input('quit now? [y]/n.')
+            if inp == 'y':
+                self.do_bye(_)
+                return True
+
+            if inp == 'n':
+                return False
+
+
+    # def preloop(self):
+    #     self.user = 'zxdewr'# input('Enter username: ')
+    #     self.passwd = 'ca1l6ack' #input('Enter password: ')
+
+    def update_cookies(self):
+        self.cookies = prepare_cookie(login(self.user, self.passwd))
+
+        for c in self.cookies:
+            self.driver.add_cookie(c)
+
+    def do_qiang(self, arg):
+        ''' Example: qiang rw TH901 382102
+            for 抢人文课号为TH901, 老师代码为382102的
+        '''
+
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(1)
+        self.driver.get(EDU_URL)
+        self.update_cookies()
+
+        try:
+            ct, cid, bsid = re.search('([a-z]{2})\s+([A-Z]{2}\d{3})\s+(\d{6})', arg).groups()
+            initpage = InitPage(self.driver)
+            tongshipage = TongshiPage(self.driver)
+            lessonpage = LessonPage(self.driver)
+            messagepage = MessagePage(self.driver)
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)
+            logging.error(e)
+            return
+
+        while True:
+            try:
+                initpage.init()
+                tongshipage.get_courses_list(ct)
+                sleep(1)
+                while True:
+                    try:
+                        tongshipage.view_lesson_by_cid(cid)
+                        lessonpage.select_by_bsid(bsid)
+                        messagepage.handle_message()
+                    except (NoSuchElementException, IndexError):
+                        assert '/speltyCommonCourse' in self.driver.current_url
+                        sleep(1)
+                        tongshipage.submit()
+                        set_trace()
+                        # self.driver.get(EDU_URL+'login.aspx')
+                        logging.info('Success')
+                        return
+
+            except AssertionError as e:
+                traceback.print_exc(file=sys.stdout)
+                if 'message=' in self.driver.current_url:
+                    messagepage.handle_message()
+                logging.error(e)
+                continue
+
+            except Exception as e:
+                if 'jaccount' in self.driver.current_url:
+                    self.update_cookies()
+                    continue
+                if 'commitConflict.aspx' in self.driver.current_url:
+                    self.driver.back()
+                    continue
+                if 'outTimePage.aspx' in self.driver.current_url:
+                    self.driver.get(EDU_URL+'login.aspx')
+                    continue
+                if 'message=' in self.driver.current_url:
+                    messagepage.handle_message()
+                    continue
+
+                traceback.print_exc(file=sys.stdout)
+                logging.error(e)
+                return 
+
+
+# TODO: Make Spiders share the cookie middleware.
+# class Main(cmd.Cmd):
+#     CURRENT_PATH = get_feed_path(CurrentSpider) # Path(get_feed_path(CurrentSpider))
+#     COURSES_PATH = get_feed_path(TongShiSpider) # Path(get_feed_path(TongShiSpider))
+#     EXPIRE_SEC = 900
+#     prompt = '>> '
+#     def __init__(self, user = 'a', passwd = 'b'):
+#         super().__init__()
+#         process = CrawlerProcess(get_project_settings())
+#         need_restart = False
+
+#         # if not self.CURRENT_PATH.is_file() or \
+#         #         time.time()-self.CURRENT_PATH.stat().st_mtime>self.EXPIRE_SEC:
+#         #     # try:
+#         #     #     os.remove(self.CURRENT_PATH)
+#         #     # except FileNotFoundError:
+#         #     #     pass
+#         #     process.crawl(CurrentSpider)
+#         #     need_restart = True
+#         #     logger.info('Preparing to update selected courses...')
+# # #         if not self.COURSES_PATH.is_file():
+# # #             process.crawl(CurrentSpider)
+# # #             need_restart = True
+# # #             logger.info('Preparing to update available courses...')
+#         # if need_restart:
+#         #     logger.info('Updating...')
+#         #     process.start()
+#         #     os.execl(sys.executable, sys.executable, *sys.argv)
+
+#         self.current = load_file(self.CURRENT_PATH)
+#         self.courses = load_file(self.COURSES_PATH)
+#         # tmp = load_file(get_feed_path(TongShiSpider))
+#         # sunzi = copy.deepcopy(tmp[0])
+#         # last = tmp[0]
+#         # for i, c in enumerate(tmp[1:]):
+#         #     tmp[i-1]['name'] = c['name']
+#         # tmp[-1]['name'] = '工程可持续发展'
+#         # self.courses = [sunzi]+tmp
+#         # logger.info('Initialization succeeded.')
+
+#     def ls_all(self):
+#         for c in self.courses:
+#             print(c['name'])
+
+#     def get_all_non_overlap(self):
+#         return {c['name'] for c in self.courses for curr in self.current if not
+#                 are_overlap(c, curr)}
+
+#         # for course in self.courses:
+#         #     for curr in self.current:
+#         #         if not are_overlap(course, curr):
+#         #             print(course['name'])
+
+#     def non_overlap_by_time(self, raw):
+#         ''' Usage: <2, 1, 3[, 1, 8][, 'o']>[;...]
+#             @return: Course names that do not overlap with any queried period,
+#                     without considering current selected courses.
+#         '''
+
+#         # TODO: add usage info
+#         queried = Main.parse_query(raw)
+#         if not queried['odd_week'] and not queried['even_week']:
+#             logger.warn('Invalid input, nothing to query.')
+#             # return set()
+#         return {c['name'] for c in self.courses if not are_overlap(c, queried)}
+
+#     def do_period(self, raw):
+#         ''' Return courses contained in your queries
+
+#             Example: >> period '2 9 10 o;4 7 8 1 8 e'
+#                     will show courses contained in the union of 
+#                     Tuesday from 9 to 10 节 from 1 to 16 odd weeks and 
+#                     Thursday from 7 to 8 节 from 1 to 8 even weeks.
+#         '''
+#         queried = Main.parse_query(raw)
+#         if not queried['odd_week'] and not queried['even_week']:
+#             logger.warn('Invalid input, nothing to query.')
+#             # print(set())
+
+#         print({c['name'] for c in self.courses if contains(queried, c)})
+
+#     def do_bye(self, arg):
+#         print('Bye')
+#         return True
+
+# # TODO: argparse
+# def main():
+#     process = CrawlerProcess(get_project_settings())
+#     print('Start')
+#     process.crawl('current')
+#     process.start()
+#     print('End')
+#     # course_info_file = Path('data/courses.json')
+#     current_courses = Path('/tmp/yuq_test_spider.json')
+#     with open('/tmp/yuq_test_spider.json', 'r') as f:
+#         courses = json.load(f)
+#     print(courses)
+#     # if not course_info_file.is_file():
+#     #     # TODO: Try to download the file from peers.
+#     #     print("Scraping target website.")
 
 if __name__ == '__main__':
     # courses = load_file('data/courses1.json')
@@ -241,7 +475,7 @@ if __name__ == '__main__':
     # c2 = courses[1]
     # print(c1, c2)
     # print(contains(c1, c2))
-    m1 = Main('a', 'b')
+    m1 = Main()
 
     m1.cmdloop()
     # print(m1.only_by_time('2 9 10 1 16 oe'))
