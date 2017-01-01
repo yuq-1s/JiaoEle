adea1c957f7652d27e24291c3f0fed8313d0df42
yuq <zxdewr@sjtu.edu.cn>
2017-01-01 22:17:04 +0800

    Add: utils.py let_logger method

--- cracker/without_browser/utils.py
+++ cracker/without_browser/utils.py
@@ -2,9 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
+import os
+import sys
+import logging
 
 ASP_FORM_ID = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION',
                '__EVENTTARGET', '__EVENTARGUMENT', '__LASTFOCUS']
+
+
+EDU_URL = 'http://electsys.sjtu.edu.cn/edu/'
+ELECT_URL = EDU_URL+'student/elect/'
+MAIN_URL=ELECT_URL+'electcheck.aspx?xklc=2'
+REMOVE_URL=ELECT_URL+'removeLessonFast.aspx'
+SUBMIT_URL=ELECT_URL+'electSubmit.aspx'
+
+def get_logger(module_name):
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARN)
+    ch.setFormatter(formatter)
+
+    LOG_PATHNAME = os.path.dirname(os.path.abspath(__file__))+'/log'
+    try:
+        os.mkdir(LOG_PATHNAME)
+    except FileExistsError:
+        pass
+    try:
+        LOG_FILENAME = sys.argv[1]+'-qiangke'
+    except IndexError:
+        LOG_FILENAME = 'qiangke'
+
+    fh = logging.FileHandler('{}/{}.log'.format(LOG_PATHNAME, LOG_FILENAME))
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+    return logger
 
 def success(resp):
     if 'message' in resp.url:
